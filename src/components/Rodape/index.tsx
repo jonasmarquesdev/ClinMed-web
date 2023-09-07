679c31f96d377033821c9356eb0814fcc436ea7e
Jonas Marques <jonas.marques2021.jm5@gmail.com>
2023-09-07 20:18:52 -0300

    Feat: Rodape

---
+++ src/components/Rodape/index.tsx
@@ -0,0 +1,58 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import styled from "styled-components";
+import facebook from "./assets/facebook.png";
+import whatsapp from "./assets/whatsapp.png";
+import google from "./assets/google.png";
+import instagram from "./assets/instagram.png";
+
+const RodapeEstilizado = styled.footer`
+  height: 100%;
+  color: white;
+  padding: 1em;
+  background-color: var(--azul-escuro);
+  text-align: center;
+`
+
+const ListaEstilizado = styled.ul`
+  display: flex;
+  justify-content: space-around;
+  width: 10%;
+  margin: 1em auto;
+  gap: 0.7em;
+`
+
+const ItemEstilizado = styled.li`
+  list-style-type: none;
+`
+
+function Rodape() {
+  return (
+    <RodapeEstilizado>
+      <ListaEstilizado>
+        <ItemEstilizado>
+          <a href="#">
+            <img src={facebook} alt="logo do facebook" />
+          </a>
+        </ItemEstilizado>
+        <ItemEstilizado>
+          <a href="#">
+            <img src={whatsapp} alt="logo do whatsapp" />
+          </a>
+        </ItemEstilizado>
+        <ItemEstilizado>
+          <a href="#">
+            <img src={google} alt="logo do google" />
+          </a>
+        </ItemEstilizado>
+        <ItemEstilizado>
+          <a href="#">
+            <img src={instagram} alt="logo do instagram" />
+          </a>
+        </ItemEstilizado>
+      </ListaEstilizado>
+      <p>2023 © Desenvolvido por Alura | Projeto fictício sem fins comerciais.</p>
+    </RodapeEstilizado>
+  );
+}
+
+export default Rodape
