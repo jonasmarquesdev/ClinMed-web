60028224ae37eba3e5be8316e52cd5ed72cd87fa
Jonas Marques <jonas.marques2021.jm5@gmail.com>
2023-09-11 17:58:07 -0300

    Fix: Otimizando imports de components

---
+++ src/components/index.tsx
@@ -0,0 +1,21 @@
+import { default as Avaliacao } from "./Avaliacao";
+import { default as Botao } from "./Botao";
+import { default as Cabecalho } from "./Cabecalho";
+import { default as Container } from "./Container";
+import { default as Grafico } from "./Grafico";
+import { default as Rodape } from "./Rodape";
+import { default as Subtitulo } from "./Subtitulo";
+import { default as Tabela } from "./Tabela";
+import { default as Titulo } from "./Titulo";
+
+export {
+  Avaliacao,
+  Botao,
+  Cabecalho,
+  Container,
+  Grafico,
+  Rodape,
+  Subtitulo,
+  Tabela,
+  Titulo,
+};
