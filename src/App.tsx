b5fac6a53c74007641dc3793eb4b43a21ba9ad92
Jonas Marques <jonas.marques2021.jm5@gmail.com>
2023-09-11 17:58:40 -0300

    Feat: Atualizando `App.tsx`

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,5 @@
 import './App.css';
-import Avaliacao from './components/Avaliacao';
-import Botao from './components/Botao';
-import Cabecalho from './components/Cabecalho';
-import Container from './components/Container';
-import Grafico from './components/Grafico';
-import Rodape from './components/Rodape';
-import Subtitulo from './components/Subtitulo';
-import Tabela from './components/Tabela';
-import Titulo from './components/Titulo';
+import { Avaliacao, Botao, Cabecalho, Container, Grafico, Rodape, Subtitulo, Tabela, Titulo } from './components';
 import useDadosConsulta from './hooks/useDadosConsulta';
 import useDadosProfissional from './hooks/useDadosProfissional';
 
